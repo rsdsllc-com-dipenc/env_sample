ce2984165100079bbeed14bd10a84f2e6e2ab9ff
Dipen Chauhan <dipenc@rsdsllc.com>
2024-04-25 09:18:36 -0700

    set welcome_msg when env unavailable

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,10 +1,21 @@
 import { Controller } from "@hotwired/stimulus";
-import { WELCOME_MSG } from "env";
+// import { WELCOME_MSG } from "env";
+import * as env from "env";
 
 export default class extends Controller {
   static targets = ["msg"];
   connect() {
-    console.log(`Welcome Msg: ${WELCOME_MSG}`);
-    this.msgTarget.textContent = WELCOME_MSG;
+    // console.log(`Welcome Msg: ${WELCOME_MSG}`);
+    // this.msgTarget.textContent = WELCOME_MSG;
+
+    console.log(Object.getOwnPropertyNames(env).sort());
+
+    let welcome_msg = "Unable to read from ENV";
+
+    if (env.WELCOME_MSG != undefined) {
+      welcome_msg = env.WELCOME_MSG;
+    }
+
+    this.msgTarget.textContent = welcome_msg;
   }
 }
